73a69b4f9c299c80d16d8517e16fd043751e8621
Muhidin <muhidin.said@hotmail.com>
2021-10-18 15:33:02 +0200

    Add the nav bar to the web page finges

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import 'leaflet/dist/leaflet.css'
 
-import { firebase } from "./initFirebase";
-import { useAuth } from "./context/AuthContext";
+import {firebase} from "./initFirebase";
+import {useAuth} from "./context/AuthContext";
 import React from 'react'
 import {Switch, Redirect} from 'react-router-dom';
 
@@ -13,66 +13,62 @@ import LinkButton from "./components/LinkButton";
 import AuthenticatedRoute from "./components/UserAuthenticatedRoute";
 import AdminRoute from "./components/AdminRoute";
 import AddPointOfInterest from "./pages/AddPointOfInterest";
+import Navigation from "./components/Nav";
 
 export const COLLECTION_POIS = "pois";
 export const COLLECTION_USERS = "users";
 
 function App() {
     // Get authenticated state using the custom "auth" hook
-    const { isAdmin } = useAuth();
-
-    // Log out of the application
-    const signOut = async () => {
-        try {
-            await firebase.auth().signOut();
-        } catch (e) {
-            console.error(e);
-        }
-    };
+    const {isAdmin, isAuthenticated} = useAuth();
 
     // Normal rendering of the app for authenticated users
     return (
-        <div className="App">
-            <h1>Welcome to the Pfyn-Finges Forest!</h1>
+        <div>
+            {isAuthenticated && <Navigation/>}
 
-            {/* Show role based on admin status (from custom claim) */}
-            <h2>Your role is : {isAdmin ? "Admin" : "User"}</h2>
+            <div className="App">
 
-            {/* Render buttons to add/remove data & log out */}
-            <div style={{ display: "flex" }}>
-                {/* Admin-only tasks */}
-                {isAdmin && (
-                    <>
-                        <LinkButton to="/admin/code/generation">Generate a QR code for a PoI</LinkButton>
-                        <LinkButton to="/admin/poi/add">Create a PoI</LinkButton>
-                    </>
-                )}
+                <h1>Welcome to the Pfyn-Finges Forest!</h1>
 
-                <button onClick={signOut}>Logout</button>
+                {/* Show role based on admin status (from custom claim) */}
+                <h2>Your role is : {isAdmin ? "Admin" : "User"}</h2>
+
+                {/* Render buttons to add/remove data & log out */}
+                <div style={{display: "flex"}}>
+                    {/* Admin-only tasks */}
+                    {isAdmin && (
+                        <>
+                            <LinkButton to="/admin/code/generation">Generate a QR code for a PoI</LinkButton>
+                            <LinkButton to="/admin/poi/add">Create a PoI</LinkButton>
+                        </>
+                    )}
+
+                </div>
+
+                <Switch>
+                    <AdminRoute path="/admin/code/generation" component={QrCodeGenerationPage}/>
+
+                    <AdminRoute path="/admin/poi/add" component={AddPointOfInterest}/>
+
+                    <AuthenticatedRoute path="/code/:code" component={CodeActivationPage}/>
+
+                    <AuthenticatedRoute path="/map/discovered-points-of-interest">
+
+                    </AuthenticatedRoute>
+
+                    <AuthenticatedRoute path="/map/walk-history/:gpx?" component={WalkHistory}/>
+
+                    <AuthenticatedRoute path="/">
+                        <Redirect to="/map/walk-history"/>
+                    </AuthenticatedRoute>
+                </Switch>
+
+                {/* Render the collection of POIs from the DB */}
+                {/*<h4>POIs Collection</h4>*/}
+                {/*<code style={{ margin: "1em", textAlign: 'left' }}><pre>{JSON.stringify(poisCollection, null, 2)}</pre></code>*/}
+
             </div>
-
-            <Switch>
-                <AdminRoute path="/admin/code/generation" component={QrCodeGenerationPage}/>
-
-                <AdminRoute path="/admin/poi/add" component={AddPointOfInterest}/>
-
-                <AuthenticatedRoute path="/code/:code" component={CodeActivationPage}/>
-
-                <AuthenticatedRoute path="/map/discovered-points-of-interest">
-
-                </AuthenticatedRoute>
-
-                <AuthenticatedRoute path="/map/walk-history/:gpx?" component={WalkHistory}/>
-
-                <AuthenticatedRoute path="/">
-                    <Redirect to="/map/walk-history"/>
-                </AuthenticatedRoute>
-            </Switch>
-
-            {/* Render the collection of POIs from the DB */}
-            {/*<h4>POIs Collection</h4>*/}
-            {/*<code style={{ margin: "1em", textAlign: 'left' }}><pre>{JSON.stringify(poisCollection, null, 2)}</pre></code>*/}
-
         </div>
     );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,50 @@
 import React, {useState} from 'react';
-import {Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Nav} from "reactstrap";
+import {Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Nav, NavbarText} from "reactstrap";
+import {firebase} from "../initFirebase";
 
 const Navigation = (props) => {
 
     const [collapsed, setCollapsed] = useState(true);
 
+    //const { isAdmin, isAuthenticated } = useAuth();
+
+    // Log out of the application
+    const signOut = async () => {
+        try {
+            await firebase.auth().signOut();
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+
     const toggleNavbar = () => setCollapsed(!collapsed);
 
-        return (
-            <div>
-                <Navbar color="faded" light>
-                    <NavbarBrand href="/" className="mr-auto">Finges Web App</NavbarBrand>
-                    <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-                    <Collapse isOpen={!collapsed} navbar>
-                        <Nav navbar>
-                            <NavItem>
-                                <NavLink href="/map/walk-history">Map Walk History</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Navbar color="faded" light expand="md">
+                <NavbarBrand href="/" className="mr-auto">Finges</NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="mr-2"/>
+                <Collapse isOpen={!collapsed} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/map/walk-history">Map Walk History</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/map/discovered-points-of-interest">Point of interest</NavLink>
+                        </NavItem>
+
+                    </Nav>
+
+
+                </Collapse>
+                <NavLink href="https://example.org">Admin</NavLink>
+                <NavbarText>
+                    <button onClick={signOut}>Logout</button>
+                </NavbarText>
+            </Navbar>
+        </div>
+    );
+}
 
 export default Navigation;

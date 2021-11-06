5137a2a70bbe5583d5482bf6dacdd838dc639424
l4p1n <l4p1n@l4p1n.ch>
2021-11-07 00:31:04 +0100

    Use state instead of a variable for the theme switcher context

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 
 const DEFAULT_THEME = 'light';
 export const THEME_COLORS = {
@@ -17,8 +17,7 @@ const ThemeContext = React.createContext(DEFAULT_THEME);
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({children}) => {
-    let theme = DEFAULT_THEME;
-    const switchTheme = (newTheme) => theme = newTheme;
+    let [theme, setTheme] = useState(DEFAULT_THEME);
 
-    return <ThemeContext.Provider value={{theme, switchTheme}}>{children}</ThemeContext.Provider>;
+    return <ThemeContext.Provider value={{theme, switchTheme: setTheme}}>{children}</ThemeContext.Provider>;
 };
